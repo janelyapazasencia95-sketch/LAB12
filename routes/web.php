57609e50d129fa2524ffc59dc564f4af11021909
janely <janelyapazasencia95@gmail.com>
2025-11-10 10:17:05 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\NotaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,5 +11,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::get('/notas', [App\Http\Controllers\NotaController::class, 'index'])->name('notas.index');
+Route::post('/notas', [App\Http\Controllers\NotaController::class, 'store'])->name('notas.store');
 Route::resource('posts', PostController::class);
