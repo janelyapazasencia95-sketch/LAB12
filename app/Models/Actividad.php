e1573d715bc6298dda0cabc8a6cae8f3f543a668
camilaangelaa76-design <camilaangelaa76@gmail.com>
2025-11-17 09:21:32 -0500

    Model Actividad

---
+++ app/Models/Actividad.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Actividad extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nota_id', 'titulo', 'descripcion', 'completado'];
+
+    public function nota()
+    {
+        return $this->belongsTo(Nota::class);
+    }
+}
